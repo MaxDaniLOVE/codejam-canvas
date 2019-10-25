d4c6c48f507b286feb32ed92b2cc713b13aef6e6
unknown <mahseeen@gmail.com>
2019-10-25 22:55:20 +0300

    feat: add onclick event

--- codejam-canvas/src/script.js
+++ codejam-canvas/src/script.js
@@ -21,3 +21,25 @@ async function fourOnFour(){
     }
 }
 //fourOnFour()
+async function thirtyTwoOnThirtyTwo(){
+    const response = await fetch('https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json')
+    const myJson = await response.json();
+    console.log(myJson[0][0]);
+ 
+    let canvas = document.querySelector('.canvas'),
+        context  = canvas.getContext('2d'),
+        width = canvas.width,
+        height = canvas.height;
+
+    for(i = 0; i <= width - width/32; i += width/32)
+    for(j = 0; j <= height - height/32; j += height/32)
+    {   
+        let color = myJson[i/(width/32)][j/(width/32)]
+        console.log(color[0]);
+        context.beginPath();
+        context.rect(j, i, width/32, width/32);
+        context.fillStyle = `rgba(${color[0]},${color[1]},${color[2]},${Math.ceil(color[3]/255)})`;
+        context.fill();
+        context.closePath();
+    }
+}
