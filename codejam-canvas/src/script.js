728011c258e5a4085317b198e7c8fc53844e3d5d
unknown <mahseeen@gmail.com>
2019-10-25 22:43:37 +0300

    feat: add onclick event

--- codejam-canvas/src/script.js
+++ codejam-canvas/src/script.js
@@ -1,4 +1,4 @@
-async function getData(){
+async function fourOnFour(){
     const response = await fetch('https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json')
     const myJson = await response.json();
     console.log(myJson[0][0]);
@@ -20,5 +20,4 @@ async function getData(){
         context.closePath();
     }
 }
-
-getData()
+//fourOnFour()
