fa4ceb7e9cb88d024823bd549f210006af1c134c
unknown <mahseeen@gmail.com>
2019-10-25 22:36:05 +0300

    feat: add 4x4 matrix

--- codejam-canvas/src/script.js
+++ codejam-canvas/src/script.js
@@ -1,23 +1,24 @@
 async function getData(){
     const response = await fetch('https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json')
     const myJson = await response.json();
-    console.log(myJson);
-    
-}
-function draw(){
+    console.log(myJson[0][0]);
+ 
     let canvas = document.querySelector('.canvas'),
         context  = canvas.getContext('2d'),
-        width = canvas.width;
+        width = canvas.width,
         height = canvas.height;
+
     for(i = 0; i <= width - width/4; i += width/4)
     for(j = 0; j <= height - height/4; j += height/4)
-    {
+    {   
+        let color = myJson[i/(width/4)][j/(width/4)]
+        console.log(i/(width/4), j/(width/4));
         context.beginPath();
         context.rect(j, i, width/4, width/4);
-        context.fillStyle = 'red';
+        context.fillStyle = `#${color}`;
         context.fill();
         context.closePath();
     }
 }
-draw()
+
 getData()
